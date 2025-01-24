ac32b3eefe204610003eeaa3c5c535ca5dce6824
Tadjibaev Madiyor <tadjibaev.madiyor@ssd.uz>
2025-01-24 17:23:59 +0500

    changed buildspec file

--- src/main/java/com/example/controller/TestController.java
+++ src/main/java/com/example/controller/TestController.java
@@ -2,16 +2,20 @@ package com.example.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
-@RequestMapping("/test")
 public class TestController {
-    @GetMapping
+
+    @GetMapping(value = "/test")
     public ResponseEntity<Map<String, String>> test(){
         return ResponseEntity.ok().body(Map.of("name", "Madiyor"));
+    }
+
+    @GetMapping(value = "/health")
+    public ResponseEntity<Map<String, String>> health(){
+        return ResponseEntity.ok().body(Map.of("status", "UP and RUNNING"));
     }
 }
